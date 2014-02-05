a9f90427e9abf5a16ac0f2a1652f9a020e9ded13
Domas <domas.bitvinskas@me.com>
2014-02-05 17:47:50 +0200

    Start implemementing paths

--- app/assets/javascripts/paths.js
+++ app/assets/javascripts/paths.js
@@ -1,1 +1,16 @@
-// Empty js file for devise controller
+var Paths = (function() {
+  var init = function() {
+    _.each($('.path'), function(box) {
+      $(box).wrap("<div class='overlay'></div>");
+    });
+  };
+
+  return {
+    init: init,
+    _private: eval(private(arguments))
+  }
+})();
+
+jQuery(function($) {
+  Paths.init();
+});
